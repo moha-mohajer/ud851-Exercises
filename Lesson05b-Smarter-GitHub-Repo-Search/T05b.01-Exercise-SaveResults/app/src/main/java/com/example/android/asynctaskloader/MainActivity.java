71b4d1a39e24292591ce5dd565db56a2898c99d8
moha-mohajer <mohajeranii@hotmail.com>
2018-01-17 16:23:48 +0200

    Lesson 5 : 13.Exercise: Persisting Data Part Two

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.asynctaskloader;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,9 +34,11 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a static final key to store the query's URL
+    // X TODO (1) Create a static final key to store the query's URL
+    private static final String  Search_querys_URL_EXTRA= "query";
 
     // TODO (2) Create a static final key to store the search's raw JSON
+    private static final String Search_raw_JSON = "results";
 
     private EditText mSearchBoxEditText;
 
@@ -60,6 +64,13 @@ public class MainActivity extends AppCompatActivity {
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         // TODO (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        if(savedInstanceState != null){
+            String queryUrl = savedInstanceState.getString(Search_querys_URL_EXTRA);
+            String rowJsonSearchResult  = savedInstanceState.getString(Search_raw_JSON);
+
+            mUrlDisplayTextView.setText(queryUrl);
+            mSearchResultsTextView.setText(rowJsonSearchResult);
+        }
     }
 
     /**
@@ -151,13 +162,26 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (3) Override onSaveInstanceState to persist data across Activity recreation
-    // Do the following steps within onSaveInstanceState
-    // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
+    // X TODO (3) Override onSaveInstanceState to persist data across Activity recreation
 
-    // TODO (5) Put the contents of the TextView that contains our URL into a variable
-    // TODO (6) Using the key for the query URL, put the string in the outState Bundle
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
 
-    // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
-    // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        // Do the following steps within onSaveInstanceState
+        // X TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
+        super.onSaveInstanceState(outState);
+
+        // X TODO (5) Put the contents of the TextView that contains our URL into a variable
+        String queryUrl = mUrlDisplayTextView.getText().toString();
+
+        // X TODO (6) Using the key for the query URL, put the string in the outState Bundle
+        outState.putString(Search_querys_URL_EXTRA,queryUrl);
+
+        // X TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
+        String rowJsonSearchResult = mSearchResultsTextView.getText().toString();
+
+        // X TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        outState.putString(Search_raw_JSON,rowJsonSearchResult);
+
+    }
 }
